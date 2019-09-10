8edc761adb0ab9a2637af740799eaa5c24ea7618
Daniel P Landon <dougedd@yahoo.com>
2019-09-09 21:46:33 -0700

    mon

--- index.js
+++ index.js
@@ -1,68 +1,72 @@
 
 
 STORE = [
-    
-{
-    question: 'Who was the first Roman Emperor?jj',
-    questionOptions: ['a.) Julius Caesarjj', 'b.) Mark Antonyjj', 'c.) Augustusjj', 'd.) Cleopatrajj'],
-    correct: 'c'
-},
 
-{
-    question: 'Who was the longest serving Roman Emperor?',
-    questionOptions: ['a.) Augustus', 'b.) Trajan', 'c.) Agrippa', 'd.) Constantine'],
-    correct: 'a'
-},
+    {
+        question: 'Who was the first Roman Emperor?jj',
+        questionOptions: ['a.) Julius Caesarjj', 'b.) Mark Antonyjj', 'c.) Augustusjj', 'd.) Cleopatrajj'],
+        correct: 'c'
+    },
 
-{
-    question: 'Who was the first Christian Roman Emperor?',
-    questionOptions: ['a.) Maxentius', 'b.) Licinius I', 'c.) Vedius', 'd.) Constantine'],
-    correct: 'd'
-},
+    {
+        question: 'Who was the longest serving Roman Emperor?',
+        questionOptions: ['a.) Augustus', 'b.) Trajan', 'c.) Agrippa', 'd.) Constantine'],
+        correct: 'a'
+    },
 
-{
-    question: 'Which Emperor Initiated the Construction of the Colosseum?',
-    questionOptions: ['a.) Titus', 'b.) Domitian', 'c.) Vespasian', 'd.) Toto'],
-    correct: 'c'
-},
+    {
+        question: 'Who was the first Christian Roman Emperor?',
+        questionOptions: ['a.) Maxentius', 'b.) Licinius I', 'c.) Vedius', 'd.) Constantine'],
+        correct: 'd'
+    },
 
-{
-    question: 'Which Emperor began the Conquest of Britain?',
-    questionOptions: ['a.) Domitian', 'b.) Claudius', 'c.) Maxentius', 'd.) Agrippa'],
-    correct: 'b'
-},
+    {
+        question: 'Which Emperor Initiated the Construction of the Colosseum?',
+        questionOptions: ['a.) Titus', 'b.) Domitian', 'c.) Vespasian', 'd.) Toto'],
+        correct: 'c'
+    },
 
-{
-    question: 'Which Roman General defeated the Carthaginians in the First Punic War?',
-    questionOptions: ['a.) Marcus Vipsanius', 'b.) Gnaeus Julius Agricola', 'c.) Diocletas', 'd.) Scipio Africanus'],
-    correct: 'd'
-},
+    {
+        question: 'Which Emperor began the Conquest of Britain?',
+        questionOptions: ['a.) Domitian', 'b.) Claudius', 'c.) Maxentius', 'd.) Agrippa'],
+        correct: 'b'
+    },
 
-{
-    question: 'Which modern day nation did the Roman General Gnaeus Julius Agricola Conquer?',
-    questionOptions: ['a.) Norway', 'b.) South Africa', 'c.) France', 'd.) Britain'],
-    correct: 'd'
-},
+    {
+        question: 'Which Roman General defeated the Carthaginians in the First Punic War?',
+        questionOptions: ['a.) Marcus Vipsanius', 'b.) Gnaeus Julius Agricola', 'c.) Diocletas', 'd.) Scipio Africanus'],
+        correct: 'd'
+    },
 
-{
-    question: 'Nero Claudius Drusus was the First Roman General to mount successful campaigns east of what River?',
-    questionOptions: ['a.) Rhine', 'b.) Danube', 'c.) Nile', 'd.) Medes'],
-    correct: 'a'
-},
+    {
+        question: 'Which modern day nation did the Roman General Gnaeus Julius Agricola Conquer?',
+        questionOptions: ['a.) Norway', 'b.) South Africa', 'c.) France', 'd.) Britain'],
+        correct: 'd'
+    },
 
-{
-    question: 'In what Manner did the Roman General Mark Antony Die?',
-    questionOptions: ['a.) Old Age', 'b.) Assassination', 'c.) Suicide', 'd.) Poison'],
-    correct: 'c'
-},
+    {
+        question: 'Nero Claudius Drusus was the First Roman General to mount successful campaigns east of what River?',
+        questionOptions: ['a.) Rhine', 'b.) Danube', 'c.) Nile', 'd.) Medes'],
+        correct: 'a'
+    },
 
-{
-    question: 'In what Manner did the Roman General Nero Claudius Drusus Die?',
-    questionOptions: ['a.) Assassination', 'b.) Falling from his horse', 'c.) Slipping on a Wet Floor', 'd.) Cuts from his Wife\'s Dagger'],
-    correct: 'b'
-},
+    {
+        question: 'In what Manner did the Roman General Mark Antony Die?',
+        questionOptions: ['a.) Old Age', 'b.) Assassination', 'c.) Suicide', 'd.) Poison'],
+        correct: 'c'
+    },
+
+    {
+        question: 'In what Manner did the Roman General Nero Claudius Drusus Die?',
+        questionOptions: ['a.) Assassination', 'b.) Falling from his horse', 'c.) Slipping on a Wet Floor', 'd.) Cuts from his Wife\'s Dagger'],
+        correct: 'b'
+    },
+
+
 
 ];
+
+var currentQuestion = 0;
 
 
 var qey = ["one"];
@@ -80,160 +84,157 @@ var y = 2;
 // goes through the questions
 
 
-$(document).ready( function() {
-    
+$(document).ready(function () {
+
     console.log('ready');
     $('.question-page').hide();
     $('.status-score').hide();
     $('.correct-feedback-page').hide();
     $('.incorrect-feedback-page').hide();
     $('.final-page').hide();
-    
-
-
-
-$('.button-startq').click(function () { //begin quiz and questions
-    $('.intro').hide();
-    $('.question-page').show();
-    $('.status-score').show();
-
-   
-});
 
 
 
 
-function generateQuestion() {
-    var i = k.length;
-    var query = qey.length;
-    var score = tally.length;
-    $(".questionQh").replaceWith(STORE[i].question);
-    $(".answerA").replaceWith(STORE[i].questionOptions[0]);
-    $(".answerB").replaceWith(STORE[i].questionOptions[1]);
-    $(".answerC").replaceWith(STORE[i].questionOptions[2]);
-    $(".answerD").replaceWith(STORE[i].questionOptions[3]);
-    console.log(`generateQuestion ran`);
-    $("p.question-status").replaceWith(`Question ${query} out of 10`);
-    $("p.current-score").replaceWith(`Score is ${score} out of 10`);
-    console.log(`Question # ${query}`);
-    console.log(`Score is ${score}`);
-    console.log(`Store place is ${i}`);
-};
+    $('.button-startq').click(function () { //begin quiz and questions
+        $('.intro').hide();
+        $('.question-page').show();
+        $('.status-score').show();
 
 
-
-
-function selectAnswer() {
-    $("#gotcha").on('click', 'label', function (event) {
-    console.log(`ANSWER IS SELECTED`);
-    let ya = $('input[name=option]:checked', '#gotcha').val();
-    console.log(ya);
-    if (ya===(STORE[i].correct)) {
-        correct();
-        
-    }
-    else {
-        wrong();
-    };
     });
 
 
 
-};
 
-
-$('.question-page').on('click', '.select-answer-right', function () {
-    $('.correct-feedback-page').show();
-    $('.question-page').hide();
-    $('.status-score').hide();
-    rr();
-    var i = k.length;
-    var query = qey.length;
-    var score = tally.length;
-    console.log(`Question # ${query}`);
-    console.log(`Score is ${score}`);
-    console.log(`Store place is ${i}`);
-    
-    
-
-});
-
-function rr() {
-    tally.push("one");
-    k.push("one");
-    qey.push("one");
-};
-
-
-
-$('.question-page').on('click', '.select-answer-wrong', function () {
-    $('.correct-feedback-page').show();
-    query ++;
-    console.log(query);
-    console.log(score);
-
-});
+    function generateQuestion(currentQuestion) {
+        console.log(currentQuestion, `current question`);
+        console.log(STORE[currentQuestion].question);
+        $(".questionQh").text(STORE[currentQuestion].question);
+        $(".answerA").text(STORE[currentQuestion].questionOptions[0]);
+        $(".answerB").text(STORE[currentQuestion].questionOptions[1]);
+        $(".answerC").text(STORE[currentQuestion].questionOptions[2]);
+        $(".answerD").text(STORE[currentQuestion].questionOptions[3]);
+        // console.log(`generateQuestion ran`);
+        $("p.question-status").text(`Question ${query} out of 10`);
+        $("p.current-score").text(`Score is ${score} out of 10`);
+        // console.log(`Question # ${query}`);
+        // console.log(`Score is ${score}`);
+        // console.log(`Store place is ${i}`);
+    };
 
 
 
 
+    function selectAnswer() {
+        $("#gotcha").on('click', 'label', function (event) {
+            // console.log(`ANSWER IS SELECTED`);
+            let ya = $('input[name=option]:checked', '#gotcha').val();
+            // console.log(ya);
+            if (ya === (STORE[i].correct)) {
+                correct();
+
+            }
+            else {
+                wrong();
+            };
+        });
+
+
+
+    };
+
+
+    $('.question-page').on('click', '.select-answer-right', function () {
+        $('.correct-feedback-page').show();
+        $('.question-page').hide();
+        $('.status-score').hide();
+        rr();
+        var i = k.length;
+        var query = qey.length;
+        var score = tally.length;
+        console.log(`Question # ${query}`);
+        console.log(`Score is ${score}`);
+        console.log(`Store place is ${i}`);
+
+
+
+    });
+
+    function rr() {
+        tally.push("one");
+        k.push("one");
+        qey.push("one");
+    };
+
+
+
+    $('.question-page').on('click', '.select-answer-wrong', function () {
+        $('.correct-feedback-page').show();
+        query++;
+        // console.log(query);
+        // console.log(score);
+
+    });
 
 
 
 
-function correct() {
-    $("div.two").replaceWith(`
+
+
+
+
+    function correct() {
+        $("div.two").html(`
         <button type='button' class="select-answer-right" role="selected-answer">Submit Answer</button>
         `);
         let y = 1;
-        console.log('correct');
-        console.log(y);
+        // console.log('correct');
+        // console.log(y);
 
-};
+    };
 
-function wrong() {
-    $("div.two").replaceWith(`
+    function wrong() {
+        $("div.two").html(`
         <button type='button' class="select-answer-wrong" role="selected-answer">Submit Answer</button>
         `);
         let y = 0;
-        console.log('not correct');
-        console.log(y);
-};
-
-
-
-$('.correct-feedback-page').on('click', '.buttonNextr', function () {
-      
-    $('.button-startq').off('click');
-    $("#gotcha").off('click');
-    $('.question-page').off('click');
-
-    if (i<10) {
-
-        generateQuestion();
-        $('.correct-feedback-page').hide();
-        $('.question-page').show();
-        $('.status-score').show();
-        
-        
-    }
-
-    else {
-        $('.final-page').show();
+        // console.log('not correct');
+        // console.log(y);
     };
-    
-});
+
+
+
+    $('.correct-feedback-page').on('click', '.buttonNextr', function () {
+
+
+
+        if (i < 10) {
+            currentQuestion++;
+            generateQuestion(currentQuestion);
+            $('.correct-feedback-page').hide();
+            $('.question-page').show();
+            $('.status-score').show();
+
+
+        }
+
+        else {
+            $('.final-page').show();
+        };
+
+    });
 
 
 
 
 
 
-function makeQuiz() {
-    generateQuestion();
-    selectAnswer();
+    function makeQuiz() {
+        generateQuestion(currentQuestion);
+        selectAnswer();
     }
-    
+
     $(makeQuiz);
 
 });
